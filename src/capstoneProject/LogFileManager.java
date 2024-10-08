6a3852f338a85b80b55a231fc72ef137dfbb5080
Allen Mwandunga <allenmcndounger@gmail.com>
2024-10-08 12:16:07 +0200

    Key Changes Made:
    LogFileManager :
    
    Implemented createLog()to create a new log file based on the specified equipment name and date.
    
    Implemented moveLog(), deleteLog(), and archiveLog()methods to manage log files as described.
    
    openLog()method returns a new instance of LogFile corresponding to the specified equipment and date.

--- src/capstoneProject/LogFileManager.java
+++ src/capstoneProject/LogFileManager.java
@@ -1,31 +1,44 @@
 package capstoneProject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 
 public class LogFileManager {
 	private String dir = "logs/"; // Log files are stored here
 	
 	// Functions to manage log files: create, move, delete...
-	
-	public void createLog(String equipmentName,LocalDate date) {
-		// Code to create log
+
+	public void createLog(String equipmentName, LocalDate date) throws IOException {
+		LogFile logFile = new LogFile(equipmentName, date);
+		if (!logFile.getFile().exists()) {
+			logFile.getFile().createNewFile();
+		}
 	}
-	
-	public void moveLog(String logPath, String destinationPath) {
-		// Code to move log
+
+	public void moveLog(String logPath, String destinationPath) throws IOException {
+		Path sourcePath = new File(logPath).toPath();
+		Path targetPath = new File(destinationPath).toPath();
+		Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
 	}
-	
-	public void deleteLog(String logPath) {
-		// Code to delete log
+
+	public void deleteLog(String logPath) throws IOException {
+		Path path = new File(logPath).toPath();
+		Files.deleteIfExists(path);
 	}
-	
-	public void archiveLog(String logPath) {
-		// Code to archive log
+
+	public void archiveLog(String logPath) throws IOException {
+		// Example of archiving by moving to a different directory
+		String archivedDir = "archive/";
+		new File(archivedDir).mkdirs(); // Create archive directory if it doesn't exist
+		moveLog(logPath, archivedDir + new File(logPath).getName());
 	}
 	
 	public File openLog(String equipmentName,LocalDate date) {
 		// Code to open log
-		return null;
+		return new LogFile(equipmentName, date);
 	}
 }
