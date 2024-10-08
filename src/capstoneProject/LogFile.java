49b599fd288bc99dc2e3e871cc556e42acb03298
Allen Mwandunga <allenmcndounger@gmail.com>
2024-10-08 12:15:26 +0200

    Key Changes Made:
    Updated file creation logic to ensure that logs are created in a predefined directory ( logs/).
    
    Implemented readData()to read log entries from the log file, appending each line to a StringBuilder.
    
    Implemented writeData()to append new log entries to the file.

--- src/capstoneProject/LogFile.java
+++ src/capstoneProject/LogFile.java
@@ -1,7 +1,11 @@
 package capstoneProject;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.time.LocalDate;
 
 public class LogFile {
@@ -13,7 +17,7 @@ public class LogFile {
 	public LogFile(String equipmentName, LocalDate date) {
 		this.equipmentName = equipmentName;
 		this.date = date;
-		this.fileName = equipmentName + "_" + date.toString() + ".log";
+		this.fileName = "logs/" + equipmentName + "_" + date.toString() + ".log";
 		this.file = new File(fileName);
 	}
 	
@@ -33,10 +37,21 @@ public class LogFile {
 	
 	// Functions to arrange dataflow
 	public String readData() throws IOException {
-		return "data"; // Create a code to read data
+		StringBuilder data = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				data.append(line).append("\n");
+			}
+		}
+		return data.toString();
 	}
 	
 	public void writeData(String data) throws IOException {
 		// Code to write data
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+			bw.write(data);
+			bw.newLine();
+		}
 	}
 }
